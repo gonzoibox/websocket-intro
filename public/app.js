291307c3494360fdfbfd723a580b0128e9c3afef
gonzoibox <gonzoibox@gmail.com>
2019-04-30 14:09:40 +0300

    start http-server

--- public/app.js
+++ public/app.js
@@ -3,6 +3,8 @@ const messages = document.getElementById('messages');
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 
+const ws = new WebSocket('ws://localhost:3000');
+
 function setStatus (value) {
     status.innerHTML = value;
 }
@@ -13,3 +15,7 @@ function printMessage(message) {
     li.innerHTML = value;
     messages.appendChild(li);
 }
+
+ws.onopen = () => setStatus('ONLINE');
+ws.onclose = () => setStatus('DISCONNECTED');
+ws.onmessage = response => printMessage(response.data);
